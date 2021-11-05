4abc21a9677551bf2daf4670d7d1354304919ebd
Brian Doyle <brian-doyle@outlook.com>
2021-11-05 17:49:54 +0000

    Move MessageQueue

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -4,11 +4,37 @@
 #include <mutex>
 #include <deque>
 #include <condition_variable>
-#include "MessageQueue.h"
 #include "TrafficObject.h"
 
 // forward declarations to avoid include cycle
 class Vehicle;
+template <class T>
+class MessageQueue
+{
+public:
+    T receive() {
+        std::unique_lock<std::mutex> lock(_mut);
+        _cond.wait(lock, [this] { return !_queue.empty(); });
+        T message = std::move(_queue.back());
+
+        _queue.pop_back();
+        return message;
+    }
+
+    void send(T &&message)
+    {
+        std::lock_guard<std::mutex> lock(_mut);
+
+        _queue.push_back(std::move(message));
+        _cond.notify_one();
+    }
+
+private:
+    std::deque<T> _queue;
+    std::condition_variable _cond;
+    std::mutex _mut;
+};
+
 
 
 // FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
