ed902cbdd3bd968789f5df04f8b7dc025cf0c6ae
Brian Doyle <brian-doyle@outlook.com>
2021-11-06 10:56:33 +0000

    Resolve merge conflicts

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -8,6 +8,38 @@
 
 // forward declarations to avoid include cycle
 class Vehicle;
+template <class T>
+class MessageQueue
+{
+public:
+    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
+    // to wait for and receive new messages and pull them from the queue using move semantics. 
+    // The received object should then be returned by the receive function. 
+    T receive() {
+        std::unique_lock<std::mutex> lock(_mut);
+        _cond.wait(lock, [this] { return !_queue.empty(); });
+        T message = std::move(_queue.back());
+
+        _queue.pop_back();
+        return message;
+    }
+
+    void send(T &&message)
+    {
+        // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
+        // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+        std::lock_guard<std::mutex> lock(_mut);
+
+        _queue.push_back(std::move(message));
+        _cond.notify_one();
+    }
+
+private:
+    std::deque<T> _queue;
+    std::condition_variable _cond;
+    std::mutex _mut;
+};
+
 
 // FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
 // Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
